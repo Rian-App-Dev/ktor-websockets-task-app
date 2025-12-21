f1d7770187bb13411b5c31b030eaf246d61ef1aa
Rian <rfrian111@gmail.com>
2025-12-21 23:12:04 +0600

    --

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -1,5 +1,7 @@
 package com.example
 
+import com.example.model.Priority
+import com.example.model.Task
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.http.content.*
@@ -8,13 +10,22 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.delay
 import java.time.Duration
 import kotlin.time.Duration.Companion.seconds
 
 fun Application.configureRouting() {
     routing {
         get("/") {
-            call.respondText("Hello World!")
+            val tasks = listOf(
+                Task("cleaning", "Clean the house", Priority.Low),
+                Task("gardening", "Mow the lawn", Priority.Medium),
+                Task("shopping", "Buy the groceries", Priority.High),
+                Task("painting", "Paint the fence", Priority.Medium)
+            )
+            for (task in tasks) {
+                call.respond(task)
+            }
         }
         // Static plugin. Try to access `/static/index.html`
         staticResources("/static", "static")
